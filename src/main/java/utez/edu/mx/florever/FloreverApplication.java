6aab88ccb39de48f47a5c4eed89e29376dc16942
MarmolejoValle <20223tn132@utez.edu.mx>
2025-07-14 16:43:24 -0600

    upload proyect springboot with all dependencies and properties

---
+++ src/main/java/utez/edu/mx/florever/FloreverApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.florever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloreverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FloreverApplication.class, args);
+    }
+
+}
